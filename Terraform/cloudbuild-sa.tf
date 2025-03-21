20b2fd65adcfbcfb4551c9860dcb5025683ca455
Abu Kalam Babuji Sadiq Basha <abukalambabuji99@gmail.com>
2025-03-21 11:21:15 -0300

    changed cloudbuild

--- Terraform/cloudbuild-sa.tf
+++ Terraform/cloudbuild-sa.tf
@@ -6,25 +6,31 @@ resource "google_service_account" "cloudbuild" {
 
 # Grant permissions to the service account
 resource "google_project_iam_member" "cloudbuild_secret_access" {
-  project = var.project_id  # Add this line
+  project = var.project_id
   role    = "roles/secretmanager.secretAccessor"
   member  = "serviceAccount:${google_service_account.cloudbuild.email}"
 }
 
 resource "google_project_iam_member" "cloudbuild_storage_admin" {
-  project = var.project_id  # Add this line
+  project = var.project_id
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.cloudbuild.email}"
 }
 
 resource "google_project_iam_member" "cloudbuild_k8s_developer" {
-  project = var.project_id  # Add this line
+  project = var.project_id
   role    = "roles/container.developer"
   member  = "serviceAccount:${google_service_account.cloudbuild.email}"
 }
 
 resource "google_project_iam_member" "cloudbuild_artifact_registry" {
-  project = var.project_id  # Add this line
+  project = var.project_id
   role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.cloudbuild.email}"
+}
+
+resource "google_project_iam_member" "cloudbuild_logging" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.cloudbuild.email}"
 }
