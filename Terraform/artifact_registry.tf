a70f9cb194c92ac3902ee083c58f09a2f834928a
Abu Kalam Babuji Sadiq Basha <abukalambabuji99@gmail.com>
2025-03-22 02:28:44 -0300

    undo 'used 2 artifact registries'

--- Terraform/artifact_registry.tf
+++ Terraform/artifact_registry.tf
@@ -1,17 +1,8 @@
-resource "google_artifact_registry_repository" "container1_repo" {
+resource "google_artifact_registry_repository" "docker_repo" {
   provider      = google
   project       = var.project_id
   location      = var.region
-  repository_id = "container1-repo"
-  description   = "Docker repository for my app"
-  format        = "DOCKER"
-}
-
-resource "google_artifact_registry_repository" "container2_repo" {
-  provider      = google
-  project       = var.project_id
-  location      = var.region
-  repository_id = "container2-repo"
+  repository_id = "my-repo"
   description   = "Docker repository for my app"
   format        = "DOCKER"
 }
