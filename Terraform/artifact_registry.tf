2a6dd3c0b32485a621929dee1379b7c8d0896f22
Abu Kalam Babuji Sadiq Basha <abukalambabuji99@gmail.com>
2025-03-22 02:05:21 -0300

    used 2 artifact registries

--- Terraform/artifact_registry.tf
+++ Terraform/artifact_registry.tf
@@ -1,8 +1,17 @@
-resource "google_artifact_registry_repository" "docker_repo" {
+resource "google_artifact_registry_repository" "container1_repo" {
   provider      = google
   project       = var.project_id
   location      = var.region
-  repository_id = "my-repo"
+  repository_id = "container1-repo"
+  description   = "Docker repository for my app"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "container2_repo" {
+  provider      = google
+  project       = var.project_id
+  location      = var.region
+  repository_id = "container2-repo"
   description   = "Docker repository for my app"
   format        = "DOCKER"
 }
